204d0d9ad753c4646a533bb6c20ad1af0d48bd41
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-09-18 13:49:15 -0400

    Model/Controllers

---
+++ RailsCards/Models/Deck.swift
@@ -0,0 +1,8 @@
+//
+//  Deck.swift
+//  RailsCards
+//
+//  Created by Bakhtiyorjon Mirzajonov on 18/09/23.
+//
+
+import Foundation
---
+++ RailsCards/Models/Flashcard.swift
@@ -0,0 +1,8 @@
+//
+//  Flashcard.swift
+//  RailsCards
+//
+//  Created by Bakhtiyorjon Mirzajonov on 18/09/23.
+//
+
+import Foundation

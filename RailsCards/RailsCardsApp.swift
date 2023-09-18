d708f1dfa98898c5db256163de869f653afedf36
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-09-18 13:28:52 -0400

    Initial Commit

---
+++ RailsCards/RailsCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailsCardsApp.swift
+//  RailsCards
+//
+//  Created by Bakhtiyorjon Mirzajonov on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RailsCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
